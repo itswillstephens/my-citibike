fc636bd072b12c348b279477bc14b79bf9180545
Will Stephens <itswillstephens@gmail.com>
2018-05-17 19:48:46 -0400

    First commit

---
+++ app.js
@@ -0,0 +1,16 @@
+$(document).ready(function() { 
+
+    var api = "https://gbfs.citibikenyc.com/gbfs/en/station_status.json";
+
+    $.getJSON("api", function(citibike) {
+        
+            let lastUpdated = citibike.data.stations[10].station_id;
+            // let bike = citibike;
+            // let stations = citibike;
+                
+            $("#last-updated").html(lastupdated);
+            // $("#bikes").html(bikes);
+            // $("#stations").html(stations);
+    })    
+
+});
