40910b3e5765608b0e31dc1701d189f233f15e61
Will Stephens <itswillstephens@gmail.com>
2018-05-17 20:30:54 -0400

    Test unix time

--- app.js
+++ app.js
@@ -7,15 +7,14 @@ $(document).ready(function() {
         const now = new Date();
         const nowUnix = now.getTime();
         let citibikeLastUpdateTime = citibike.data.stations[641].last_reported;
-
-
+        let lastUpdated = (nowUnix - citibikeLastUpdateTime) * 1000;
+        console.log(nowUnix);
+        console.log(citibikeLastUpdateTime);
 
         let stationID = citibike.data.stations[641].station_id;
-        let lastUpdated = (nowUnix - citibikeLastUpdateTime) * 1000;
         let bikes = citibike.data.stations[641].num_bikes_available;
         let docks = citibike.data.stations[641].num_docks_available;
 
-            console.log(stationID);
             $("#last-updated").html(lastUpdated);
             $("#bikes").html(bikes);
             $("#docks").html(docks);
