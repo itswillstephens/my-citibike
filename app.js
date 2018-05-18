f67447dca8479b85b19a7bad8b2d347ba0c8c08c
Will Stephens <itswillstephens@gmail.com>
2018-05-17 20:02:38 -0400

    v1.3

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 $(document).ready(function() { 
 
-    let api;
+    let api = "https://gbfs.citibikenyc.com/gbfs/en/station_status.json";
+    
     $.getJSON(api, function(citibike) {
         
-        api = "https://gbfs.citibikenyc.com/gbfs/en/station_status.json";
         let lastUpdated = citibike.data.stations[10].station_id;
             // let bike = citibike;
             // let stations = citibike;
