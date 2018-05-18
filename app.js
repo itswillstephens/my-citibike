5e42f033aaf08436dc02d48f2696fa4ac33792b1
Will Stephens <itswillstephens@gmail.com>
2018-05-17 20:29:35 -0400

    Fix unix time

--- app.js
+++ app.js
@@ -4,8 +4,14 @@ $(document).ready(function() {
 
     $.getJSON(api, function(citibike) {
         
+        const now = new Date();
+        const nowUnix = now.getTime();
+        let citibikeLastUpdateTime = citibike.data.stations[641].last_reported;
+
+
+
         let stationID = citibike.data.stations[641].station_id;
-        let lastUpdated = citibike.data.stations[641].last_reported;
+        let lastUpdated = (nowUnix - citibikeLastUpdateTime) * 1000;
         let bikes = citibike.data.stations[641].num_bikes_available;
         let docks = citibike.data.stations[641].num_docks_available;
 
