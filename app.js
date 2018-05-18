c137bb977aec34fe5354715b095e7652eae90386
Will Stephens <itswillstephens@gmail.com>
2018-05-17 20:24:51 -0400

    Add station ID

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ $(document).ready(function() {
 
     $.getJSON(api, function(citibike) {
         
-        console.log(api);
+        let stationID = citibike.data.stations[640].station_id;
         let lastUpdated = citibike.data.stations[640].last_reported;
         let bikes = citibike.data.stations[640].num_bikes_available;
         let docks = citibike.data.stations[640].num_docks_available;
-                
+
+            $("#station-id").html(stationID);
             $("#last-updated").html(lastUpdated);
             $("#bikes").html(bikes);
             $("#docks").html(docks);
