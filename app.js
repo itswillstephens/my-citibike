a4f39886d439879014d2e6b9b680e34029dad665
Will Stephens <itswillstephens@gmail.com>
2018-05-17 20:16:13 -0400

    Hook up bikes, docks, and last updated timestamp

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ $(document).ready(function() {
     $.getJSON(api, function(citibike) {
         
         console.log(api);
-        let lastUpdated = citibike.data.stations[640].station_id;
-            // let bike = citibike;
-            // let stations = citibike;
+        let lastUpdated = citibike.data.stations[640].last_reported;
+        let bike = citibike.data.stations[640].num_bikes_available;
+        let stations = citibike.data.stations[640].num_docks_available;
                 
             $("#last-updated").html(lastUpdated);
-            // $("#bikes").html(bikes);
-            // $("#stations").html(stations);
+            $("#bikes").html(bikes);
+            $("#stations").html(stations);
     })    
 
 });
